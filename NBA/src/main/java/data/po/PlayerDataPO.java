e67425e72f469735f102889c7f7082b89a551a87
thometoy <29884060@qq.com>
2015-03-08 23:08:55 +0800

    po   vo
    PO\VO

--- NBA/src/main/java/data/po/PlayerDataPO.java
+++ NBA/src/main/java/data/po/PlayerDataPO.java
@@ -10,4 +10,69 @@ public class PlayerDataPO {
 	int Age;//年龄
 	int Exp;//球龄
 	String school;//学校
+	String TeamName;//队伍
+	int GP;//比赛场数
+	int GS;//先发场数
+	
+	int backboard;//总篮板
+	double BPG;//场均篮板
+	
+	int assist;//助攻数
+	double APG;// 场均助攻
+	
+	double MinutesOnField;//在场时间
+	double MPG;//场均时间
+	
+	double FieldGoalPercentage;//投篮命中率
+	
+	double ThreePGPercentage;//三分命中率
+	
+	double FTPercentage;//罚球命中率，Free Throw
+	
+	int Off;//进攻数
+	double OffPG;//场均
+	
+	int Def;//防守数
+	double DefPG;//场均
+	
+	int Steal;//抢断数
+	double StealPG;//场均抢断
+	
+	int Rejection;//盖帽数
+	double RPG;//场均盖帽
+	
+	int To;//失误
+	double ToPG;//场均失误
+	
+	int foul;//犯规
+	double foulPG;//场均犯规
+	
+	int PTS;//得分
+	double PPG;//场均得分
+	
+	double Eff;//效率
+	
+	double Gmsc;//GMSC效率
+	
+	double TruePercentage;//真实命中率
+	
+	double ShootEff;//投篮效率，
+	
+	double BackboardEff;//篮板率，
+	
+	double OffBEff;//进攻篮板率，
+	
+	double DefBEff;//防守篮板率，
+	
+	double AssitEff;//助攻率，
+	
+	double StealEff;//抢断率，
+	
+	double RejectionEff;//盖帽率，
+	
+	double ToEff;//失误率，
+	
+	double UseEff;//使用率
+
+	
 }
--- NBA/src/main/java/data/po/TeamDataPO.java
+++ NBA/src/main/java/data/po/TeamDataPO.java
@@ -8,4 +8,76 @@ public class TeamDataPO {
 	String area;//分区
 	String mainposition;//场馆
 	int buildyear;//年份
+	
+	int MatchNumber;//比赛场数
+	
+	int ShootNumber;//投篮数
+	double ShootNumberPG;//场均投篮
+	
+	int ShootEffNumber;//投篮命中数
+	double ShootEffNumberPG;//场均
+	
+	int TPNumber;//三分出手数
+	double TPNumberPG;//场均
+	
+	int TPEffNumber;//三分命中数
+	double TPEffNumberPG;//场均
+	
+	int FTEffNumber;//罚球命中数
+	double FTEffNumberPG;//场均
+	
+	int FTNumber;//罚球出手数
+	double FTNumberPG;//场均
+	
+	int OffBackBoard;//进攻篮板
+	double OffBackBoardPG;//场均
+	
+	int DefBackBoard;//防守篮板
+	double DefBackBoradPG;//场均
+	
+	int BackBoard;//篮板数
+	double BackBoardPG;//场均
+	
+	int AssitNumber;//助攻数
+	double AssitNumberPG;
+	
+	int StealNumber;//抢断数
+	double StealNumberPG;
+	
+	int Rejection;//盖帽数，
+	double RejectionPG;
+	
+	int To;//失误数
+	double ToPG;
+	
+	int Foul;//犯规数，
+	double FoulPG;
+	
+	int PTS;//比赛得分，
+	double PPG;
+	
+	double ShootEff;//投篮命中率，
+	
+	double TPEff;//三分命中率，
+	
+	double FTEff;//罚球命中率，
+	
+	double WR;//胜率，
+	
+	int Off;//进攻回合，
+	double OffPG;
+	
+	int Def;//防守回合，
+	double DefPG;
+	
+	double OffEff;//进攻效率，
+	
+	double DefEff;//防守效率，
+	
+	double BackBoardEff;//篮板效率，
+	
+	double StealEff;//抢断效率，
+	
+	double AssistEff;//助攻率
+	
 }
