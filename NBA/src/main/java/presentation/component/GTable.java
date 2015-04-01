a2bd424ab6f13ff95ba66cca2609e4f0661caad4
GraveParty <770637642@qq.com>
2015-04-01 20:57:54 +0800

    ui
    八阿哥修好啦！！！！！！！！

--- NBA/src/main/java/presentation/component/GTable.java
+++ NBA/src/main/java/presentation/component/GTable.java
@@ -1,5 +1,6 @@
 package presentation.component;
 
+import java.awt.Cursor;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.util.Enumeration;
@@ -21,155 +22,168 @@ public class GTable extends JTable{
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	
+
 	public JButton pageUp;
 	public JButton pageDown;
 	public JTextField page;
-	
+
 	public int sortingCol = 0;  //用于标记被排序的列
 
 	public boolean fit = true;
-	
+
 	public GTable(PagingTableModel dm,JButton up,JButton down,JTextField p){
 		this(dm,32,30,up,down,p);
 	}
-	
+
 	public GTable(PagingTableModel dm,JButton up,JButton down,JTextField p,boolean sortable){
 		this(dm,32,30,up,down,p,sortable);
 	}
 
-	
-	
+
+
 	public GTable(final PagingTableModel dm,int rowHeight,int headerHeight,JButton up,JButton down,JTextField p){
 		super(dm);
-		
+
 		pageUp = up;
 		pageDown = down;
 		page = p;
-		
+
 		this.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
 
 		TableUtility.setFont(this);
 		TableUtility.setTableColor(this);
 		TableUtility.setTableRowHeight(this, rowHeight);
 		TableUtility.setTableHeaderHeight(this, headerHeight);
-		
-		
-//		int col = this.getColumnModel().getColumnCount();
-//		for(int i=0;i<col;i++){
-//			this.getColumnModel().getColumn(i).setPreferredWidth(50);
-//			this.getColumnModel().getColumn(i).setMaxWidth(50);
-//			this.getColumnModel().getColumn(i).setMinWidth(50);
-//		}
-		
+
+
+		//		int col = this.getColumnModel().getColumnCount();
+		//		for(int i=0;i<col;i++){
+		//			this.getColumnModel().getColumn(i).setPreferredWidth(50);
+		//			this.getColumnModel().getColumn(i).setMaxWidth(50);
+		//			this.getColumnModel().getColumn(i).setMinWidth(50);
+		//		}
+
 		fitTableColumns(this);
 
 		this.getTableHeader().addMouseListener(new HeaderListener());
 
 	}
-	
+
 	public GTable(final PagingTableModel dm,int rowHeight,int headerHeight,JButton up,JButton down,JTextField p,boolean sortable){
 		super(dm);
-		
+
 		pageUp = up;
 		pageDown = down;
 		page = p;
-		
+
 		fit = sortable;
-		
+
 		this.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
 
 		TableUtility.setFont(this);
 		TableUtility.setTableColor(this);
 		TableUtility.setTableRowHeight(this, rowHeight);
 		TableUtility.setTableHeaderHeight(this, headerHeight);
-		
+
 		if(fit){
 			fitTableColumns(this);
 		}
-		
-//		this.getTableHeader().addMouseListener(new HeaderListener());
+
+		//		this.getTableHeader().addMouseListener(new HeaderListener());
 
 	}
-	
+
 	public void fitTableColumns(JTable myTable){
-         myTable.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
-         JTableHeader header = myTable.getTableHeader();
-         int rowCount = myTable.getRowCount();
-         Enumeration columns = myTable.getColumnModel().getColumns();
-         while(columns.hasMoreElements())
-         {
-             TableColumn column = (TableColumn)columns.nextElement();
-             int col = header.getColumnModel().getColumnIndex(column.getIdentifier());
-             int width = (int)header.getDefaultRenderer().getTableCellRendererComponent
-             (myTable, column.getIdentifier(), false, false, -1, col).getPreferredSize().getWidth();
-             for(int row = 0; row < rowCount; row++)
-             {
-                 int preferedWidth = (int)myTable.getCellRenderer(row, col).getTableCellRendererComponent
-                 (myTable, myTable.getValueAt(row, col), false, false, row, col).getPreferredSize().getWidth();
-                 width = Math.max(width, preferedWidth);
-             }
-             header.setResizingColumn(column); // 此行很重要
-             column.setWidth(width+myTable.getIntercellSpacing().width);
-         }
-    }
-	
-	
-	
-	class HeaderListener implements MouseListener{
-
-		public void mouseClicked(MouseEvent e) {
-			JTableHeader header = GTable.this.getTableHeader();
-			PagingTableModel model = (PagingTableModel)GTable.this.getModel();
-			int tableColumn = header.columnAtPoint(e.getPoint());//获取点击的列
-			
-			if(tableColumn!=0){  //第一列为序号不排序
-				if(tableColumn == sortingCol){  //表示第二次点击，升序
-					GTable.this.setModel(new PagingTableModel(TableData.ascendingSort(model.data, tableColumn)));
-					sortingCol = 0;
-				}else{  //第一次点击，降序
-					GTable.this.setModel(new PagingTableModel(TableData.descendingSort(model.data, tableColumn)));
-					sortingCol = tableColumn;
-				}
-				
-				//重新设置翻页按钮
-				PagingTableModel.setPagingButton(GTable.this, pageUp, pageDown);
-				
-				//列宽自适应
-				if(fit){
-					fitTableColumns(GTable.this);
-					GTable.this.repaint();
-				}
-				
-				//更改页数
-				model = (PagingTableModel)GTable.this.getModel();
-				page.setText(String.valueOf(model.getPageOffset()+1));
-				
-				MainUI.getMainFrame().repaint();
+		myTable.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+		JTableHeader header = myTable.getTableHeader();
+		int rowCount = myTable.getRowCount();
+		Enumeration columns = myTable.getColumnModel().getColumns();
+		while(columns.hasMoreElements())
+		{
+			TableColumn column = (TableColumn)columns.nextElement();
+			int col = header.getColumnModel().getColumnIndex(column.getIdentifier());
+			int width = (int)header.getDefaultRenderer().getTableCellRendererComponent
+					(myTable, column.getIdentifier(), false, false, -1, col).getPreferredSize().getWidth();
+			for(int row = 0; row < rowCount; row++)
+			{
+				int preferedWidth = (int)myTable.getCellRenderer(row, col).getTableCellRendererComponent
+						(myTable, myTable.getValueAt(row, col), false, false, row, col).getPreferredSize().getWidth();
+				width = Math.max(width, preferedWidth);
 			}
+			header.setResizingColumn(column); // 此行很重要
+			column.setWidth(width+myTable.getIntercellSpacing().width);
 		}
-
-		public void mouseEntered(MouseEvent arg0) {
-			// TODO Auto-generated method stub
-			
-		}
-
-		public void mouseExited(MouseEvent arg0) {
-			// TODO Auto-generated method stub
-			
-		}
-
-		public void mousePressed(MouseEvent arg0) {
-			// TODO Auto-generated method stub
-			
-		}
-
-		public void mouseReleased(MouseEvent arg0) {
-			// TODO Auto-generated method stub
-			
-		}
-		
 	}
+
+//	protected JTableHeader createDefaultTableHeader() {   
+//		return new JTableHeader(columnModel) {   
+//			public String getToolTipText(MouseEvent e) {   
+//				String tip = null;   
+//				java.awt.Point p = e.getPoint();   
+//				int index = columnModel.getColumnIndexAtX(p.x);   
+//				int realIndex =  columnModel.getColumn(index).getModelIndex();   
+//				return "点击降序/升序";   
+//			}   
+//		};   
+//	}   
+
+
+
+
+class HeaderListener implements MouseListener{
+
+	public void mouseClicked(MouseEvent e) {
+		JTableHeader header = GTable.this.getTableHeader();
+		PagingTableModel model = (PagingTableModel)GTable.this.getModel();
+		int tableColumn = header.columnAtPoint(e.getPoint());//获取点击的列
+
+		if(tableColumn!=0){  //第一列为序号不排序
+			if(tableColumn == sortingCol){  //表示第二次点击，升序
+				GTable.this.setModel(new PagingTableModel(TableData.ascendingSort(model.data, tableColumn)));
+				sortingCol = 0;
+			}else{  //第一次点击，降序
+				GTable.this.setModel(new PagingTableModel(TableData.descendingSort(model.data, tableColumn)));
+				sortingCol = tableColumn;
+			}
+
+			//重新设置翻页按钮
+			PagingTableModel.setPagingButton(GTable.this, pageUp, pageDown);
+
+			//列宽自适应
+			if(fit){
+				fitTableColumns(GTable.this);
+				GTable.this.repaint();
+			}
+
+			//更改页数
+			model = (PagingTableModel)GTable.this.getModel();
+			page.setText(String.valueOf(model.getPageOffset()+1));
+
+			MainUI.getMainFrame().repaint();
+		}
+	}
+
+	public void mouseEntered(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		GTable.this.getTableHeader().setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+	}
+
+	public void mouseExited(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void mousePressed(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
 
 
 }
--- NBA/src/main/java/presentation/contenui/TableUtility.java
+++ NBA/src/main/java/presentation/contenui/TableUtility.java
@@ -15,7 +15,7 @@ public class TableUtility {
 	}
 	
 	public static void setTableHeaderHeight(JTable table,int h){
-		table.getTableHeader().setPreferredSize(new Dimension(table.getTableHeader().getWidth(),h));
+		table.getTableHeader().setPreferredSize(new Dimension(table.getTableHeader().getPreferredSize().width,h));
 	}
 	
 	public static void setTabelPanel(JScrollPane tablePanel){
@@ -39,4 +39,94 @@ public class TableUtility {
 		table.getTableHeader().setForeground(Color.white);
 	}
 
+	public static String getChPosition(String en){
+		if(en.contains("-")){
+			String[] pos = en.split("-");
+			System.out.println(pos[0]+pos[1]);
+			return transPosition(pos[0])+"-"+transPosition(pos[1]);
+		}else{
+			return transPosition(en);
+		}
+	}
+	
+	private static String transPosition(String en){
+		if(en=="G"){
+			return "后卫";
+		}else if(en=="F"){
+			return "前锋";
+		}else if(en=="C"){
+			return "中锋";
+		}else{
+			return "~~~";
+		}
+	}
+	
+	public static String getChTeam(String en){
+		String ch;
+		if(en=="SAS"){
+			ch = "圣安东尼奥马刺";
+		}else if(en=="MEM"){
+			ch = "孟菲斯灰熊";
+		}else if(en=="HOU"){
+			ch = "休斯顿火箭";
+		}else if(en=="DAL"){
+			ch = "达拉斯小牛";
+		}else if(en=="NOP"){
+			ch = "新奥尔良鹈鹕";
+		}else if(en=="SAC"){
+			ch = "萨克拉门托国王";
+		}else if(en=="PHX"){
+			ch = "菲尼克斯太阳";
+		}else if(en=="GSW"){
+			ch = "金州勇士";
+		}else if(en=="LAC"){
+			ch = "洛杉矶快船";
+		}else if(en=="MIN"){
+			ch = "明尼苏达森林狼";
+		}else if(en=="DEN"){
+			ch = "犹他爵士";
+		}else if(en=="POR"){
+			ch = "波特兰开拓者";
+		}else if(en=="OTC"){
+			ch = "俄克拉荷马城雷霆";
+		}else if(en=="MIA"){
+			ch = "迈阿密热火";
+		}else if(en=="ORL"){
+			ch = "奥兰多魔术";
+		}else if(en=="ATL"){
+			ch = "亚特兰大老鹰";
+		}else if(en=="WAS"){
+			ch = "华盛顿奇才";
+		}else if(en=="CHA"){
+			ch = "夏洛特黄蜂";
+		}else if(en=="DET"){
+			ch = "底特律活塞";
+		}else if(en=="IND"){
+			ch = "印第安纳步行者";
+		}else if(en=="CLE"){
+			ch = "克利夫兰骑士";
+		}else if(en=="CHI"){
+			ch = "芝加哥公牛";
+		}else if(en=="MIL"){
+			ch = "密尔沃基雄鹿";
+		}else if(en=="NYN"){
+			ch = "纽约尼克斯";
+		}else if(en=="PHI"){
+			ch = "费城76人";
+		}else if(en=="CEL"){
+			ch = "波士顿凯尔特人";
+		}else if(en=="BKN"){
+			ch = "布鲁克林篮网";
+		}else if(en=="TOR"){
+			ch = "多伦多猛龙";
+		}else{
+			ch = "!!!";
+		}
+		System.out.println(ch);
+		return en+" "+ch;
+	}
+	
+
+	
+	
 }
